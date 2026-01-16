0b17e3c36e4fdf0c56194f90b702e1fba6950347
Jessica Kiroff <jesskiroff@gmail.com>
2026-01-16 13:31:40 -0500

    complete turtle game

--- turtle-race.py
+++ turtle-race.py
@@ -1,44 +1,42 @@
 from turtle import Turtle, Screen
+import random
 
 
+is_race_on = False
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 y_positions = [-70, -40, -10, 20, 50, 80]
-
+all_turtles = []
 
 
 for turtle_index in range(0, len(colors)):
-    tim = Turtle(shape="turtle")
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[turtle_index])
-    tim.color(colors[turtle_index])
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle.color(colors[turtle_index])
+    all_turtles.append(new_turtle)
+
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner!")
+
+        turtle.forward(random.randint(0, 10))
     
 
-# def forward():
-#     tim.forward(10)
-
-# def backward():
-#     tim.backward(10)
-
-# def left():
-#     tim.left(10)
-
-# def right():
-#     tim.right(10)
 
 
-# def reset():
-#     tim.clear()
-#     tim.penup()
-#     tim.home()
-#     tim.pendown()
 
-# screen.listen()
-# screen.onkey(forward, "f")
-# screen.onkey(backward, "b")
-# screen.onkey(left, "l")
-# screen.onkey(right, "r")
-# screen.onkey(reset, "c")
 screen.exitonclick()
